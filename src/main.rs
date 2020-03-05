7fba171314022a59dead5cb8d2480d19ad50f745
Rafael Epplee <raffomania@gmail.com>
2020-03-05 22:53:16 +0100

    add example request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::client::Client;
+
+#[actix_rt::main]
+async fn main() {
+    let mut client = Client::default();
+
+    let response = client.get("https://doc.rust-lang.org/stable/book/ch03-01-variables-and-mutability.html")
+        .send()
+        .await;
+
+    println!("Response: {:?}", response);
 }

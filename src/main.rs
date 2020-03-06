729e36f9708bab374ad401c9dd78287ba20b004e
Rafael Epplee <raffomania@gmail.com>
2020-03-06 21:51:24 +0100

    working readability demo

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-use actix_web::client::Client;
+use actix_web::client::{Client, SendRequestError};
+use bytes::buf::BufExt;
+use readability::extractor;
+use url::Url;
+use std::env;
 
 #[actix_rt::main]
 async fn main() {
-    let mut client = Client::default();
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        println!("arg {}", args[1]);
+        scrape(&args[1]).await;
+    } else {
+        println!("please provide an url");
+    }
+}
 
-    let response = client.get("https://doc.rust-lang.org/stable/book/ch03-01-variables-and-mutability.html")
+async fn scrape(url: &String) -> Result<(), SendRequestError> {
+    let client = Client::default();
+
+    let mut response = client.get(url)
         .send()
-        .await;
+        .await?;
 
-    println!("Response: {:?}", response);
+    let status = response.status();
+    println!("status {}", status);
+
+    let body = response.body().await.unwrap();
+
+    let extracted = extractor::extract(&mut body.reader(), &Url::parse(url).unwrap()).unwrap();
+
+    println!("{:?}", extracted.text);
+    Ok(())
 }

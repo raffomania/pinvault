65453fcb6e1ac41aecb2437c0e8f70365f892888
Rafael Epplee <raffomania@gmail.com>
2020-05-03 20:38:09 +0200

    keep track of running downloads

--- src/download.rs
+++ src/download.rs
@@ -5,42 +5,73 @@ use crossbeam::channel::{unbounded, Receiver, Sender};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 use duct::cmd;
+use std::collections::HashMap;
 use std::env;
 use std::thread;
+use uuid::Uuid;
 
 use crate::schema;
 use crate::web::DbPool;
 
 pub enum Msg {
     Download(String),
+    RequestInfo,
 }
 
-pub fn start_download_thread() -> Sender<Msg> {
-    let (sender, receiver) = unbounded::<Msg>();
+pub enum Info {
+    Downloads(Vec<Download>),
+}
+
+#[derive(Clone)]
+pub struct Download {
+    url: String,
+    finished: bool,
+    error: Option<String>,
+}
+
+pub fn start_download_thread() -> (Sender<Msg>, Receiver<Info>) {
+    let (req_sender, req_receiver) = unbounded::<Msg>();
+    let (info_sender, info_receiver) = unbounded::<Info>();
 
     thread::spawn(|| {
-        handle_messages(receiver);
+        handle_messages(info_sender, req_receiver);
     });
 
-    sender
+    (req_sender, info_receiver)
 }
 
-pub fn handle_messages(receiver: Receiver<Msg>) {
+pub fn handle_messages(sender: Sender<Info>, receiver: Receiver<Msg>) {
     let db_url = env::var("DATABASE_URL").expect("Please set DATABASE_URL");
     let manager = ConnectionManager::<SqliteConnection>::new(db_url);
     let pool = r2d2::Pool::builder()
         .build(manager)
         .expect("Failed to create DB connection pool");
+    let mut downloads: HashMap<Uuid, Download> = HashMap::new();
 
     while let Ok(msg) = receiver.recv() {
         match msg {
-            Msg::Download(url) => download(&pool, url),
+            Msg::Download(url) => {
+                download(&pool, &mut downloads, url);
+            }
+            RequestInfo => {
+                let downloads: Vec<Download> = downloads.values().cloned().collect();
+                sender.send(Info::Downloads(downloads));
+            }
         };
     }
 }
 
-pub fn download(pool: &DbPool, url: String) -> Result<()> {
+pub fn download(pool: &DbPool, downloads: &mut HashMap<Uuid, Download>, url: String) -> Result<()> {
+    let id = Uuid::new_v4();
+    let mut download = Download {
+        url: url.clone(),
+        finished: false,
+        error: None,
+    };
+    downloads.insert(id, download.clone());
     save_file(pool, ytdl(&url)?)?;
+    download.finished = true;
+    downloads.insert(id, download);
     Ok(())
 }
 
